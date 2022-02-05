36e90b9a1279681f418e1e960615e8fcf9f9a7a3
kratikakeer <keerkratika212@gmail.com>
2022-02-05 10:03:49 +0530

    new update

--- employee/src/main/java/com/example/demo/controller/Controller.java
+++ employee/src/main/java/com/example/demo/controller/Controller.java
@@ -1,5 +1,12 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+	@Autowired
+	Service service;
 
 }
--- employee/src/main/java/com/example/demo/model/Employee.java
+++ employee/src/main/java/com/example/demo/model/Employee.java
@@ -1,5 +1,19 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+@Data
+@Entity
 public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String name;
+	private String designation;
+	private double salary;
+	private int age;
 
 }
